bf3e722c6810c26e4b02d1b5e226fe0a055d86f5
Vamsy <vamsy@Vamsys-MacBook-Air.local>
2025-04-20 14:45:00 +0530

    FEATURE: The very first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+const heading = React.createElement(
+  "div",
+  {
+    id: "heading",
+  },
+  [
+    React.createElement(
+      "h1",
+      {
+        id: "headingH1",
+      },
+      "hello from h1"
+    ),
+    React.createElement("h4", { id: "headingH2" }, "hello from h4"),
+  ]
+);
+console.log(React);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(ReactDOM);
+
+root.render(heading);
